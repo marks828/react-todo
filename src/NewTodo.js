0a47e4f7235eb2c83edf5575352ff6a92a2a8c41
Mark Sweet <marksweet@Marks-MacBook-Air.local>
2021-08-12 11:17:13 -0400

    start todo app

---
+++ src/NewTodo.js
@@ -0,0 +1,9 @@
+function NewTodo() {
+    return(
+        <label type="text" name="New To Do" for="new_to_do">
+            <input type="text" name="new_to_do" placeholder="To Do"></input>
+        </label>
+    )
+}
+
+export default NewTodo;
---
+++ src/todo.js
@@ -0,0 +1,10 @@
+import NewTodo from "./NewTodo";
+
+function Todo() {
+    return(
+       
+        <NewTodo />
+    );
+}
+
+export default Todo;
