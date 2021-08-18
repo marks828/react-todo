06aec52d17890ced0c6ba9f34a8b3d4499bbdbaa
Mark Sweet <marksweet@Marks-MacBook-Air.local>
2021-08-18 15:32:48 -0400

    update git files

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,17 +1,27 @@
 import NewTodo from "./NewTodo";
 import { dummyData} from "../dummydata/dummyData"
+import { useEffect, useState } from "react";
 
 function Todo() {
-    const dummyDataArray = [];
+
+
+    const [todos, settodos] = useState();
+    const [count, setCount] = useState(0);
+
     return(
     <> 
         <ul>
             {dummyData.map((todoItem) =>
-                <li>{todoItem.todoDescription}
+                <li>
+                    {todoItem.todoDescription}
                     {console.log(todoItem)}
+                    
                 </li>
             )}
         </ul>
+        {count}
+        <button onClick={() => setCount(count + 1)}>Count</button>
+        <button onClick={() => setCount(0)}>reset</button>
         <NewTodo />
     </>
     );
