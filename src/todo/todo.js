0cf144229735f8f026a240213d6dc47a766a84b2
Mark Sweet <marksweet@Marks-MacBook-Air.local>
2021-10-01 14:23:26 -0400

    update edit function

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,90 +1,82 @@
-import { dummyData } from "../dummydata/dummyData";
+import { dummyData } from "../dummydata/dummyData"
 import { useState } from "react";
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid'
 
 function Todo(props) {
-	const [todos, setTodos] = useState(dummyData);
-	const [input, setInput] = useState("");
-	const [isEditing, setIsEditing] = useState(false);
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		setInput("");
-	}
+    const [todos, setTodos] = useState(dummyData);
+    const [input, setInput] = useState('')
+    const [isEditing, setIsEditing] = useState(false)
 
-	function toggleTodoCompleted(id) {
-		const updatedTodos = todos.map((todo) => {
-			if (id === todo.id) {
-				return { ...todo, completed: !todo.completed };
-			}
-			return todo;
-		});
-		console.log(updatedTodos);
-		setTodos(updatedTodos);
-	}
+    function handleSubmit(e) {
+        e.preventDefault();
+        setInput('')
+    }
 
-	function deleteTodo(id) {
-		const deleteTodoFromList = todos.filter((todo) => id !== todo.id);
-		setTodos(deleteTodoFromList);
-	}
+    function toggleTodoCompleted(id) {
+        const updatedTodos = todos.map(todo => {
+            if (id === todo.id) {
+                return { ...todo, completed: !todo.completed }
+            }
+            return todo;
+        });
+        console.log(updatedTodos)
+        setTodos(updatedTodos)
+    }
 
-	function updateEditingStatus(id) {
-		const editedTodoList = todos.map((todo) => {
-			if (id === todo.id) {
-				return { ...todo, isEditing: !todo.isEditing };
-			}
-			return todo;
-		});
-		console.log(editedTodoList);
-		return setTodos(editedTodoList);
-	}
+    function deleteTodo(id) {
+        const deleteTodoFromList = todos.filter(todo => id !== todo.id)
+        setTodos(deleteTodoFromList)
+    }
 
-	return (
-		<>
-			<ul>
-				{todos.map((todoItem) => {
-					todoItem.isEditing ? (
-						<li>
-							<p>hello</p>{" "}
-						</li>
-					) : (
-						<li>
-							<p
-								id={todoItem.id}
-								className={todoItem.completed ? "completed" : "not-completed"}>
-								<input
-									type='checkbox'
-									onChange={() => toggleTodoCompleted(todoItem.id)}></input>
-								{todoItem.todoDescription}
-							</p>
-							<button onClick={() => updateEditingStatus(todoItem.id)}>
-								Edit
-							</button>
-							<button onClick={() => deleteTodo(todoItem.id)}>Delete</button>
-						</li>
-					);
-				})}
-			</ul>
-			<form onSubmit={handleSubmit}>
-				<label type='text' name='New To Do' for='new_to_do'>
-					<p>New Todo</p>
-					<input
-						type='text'
-						name='new_to_do'
-						placeholder='To Do'
-						value={input}
-						onChange={(event) => setInput(event.target.value)}></input>
-				</label>
-				<button
-					id='add-new-task'
-					onClick={() =>
-						setTodos([...todos, { todoDescription: input, id: nanoid() }])
-					}>
-					add to do
-				</button>
-			</form>
-		</>
-	);
+    function updateEditingStatus(id) {
+        const editedTodoList = todos.map(todo => {
+            if (id === todo.id) {
+                return { ...todo, isEditing: !todo.isEditing }
+            };
+            return todo
+        })
+        console.log(editedTodoList)
+        return setTodos(editedTodoList)
+    }
+
+    return (
+        <>
+            <ul>
+                {todos.map((todoItem) =>
+                    todoItem.isEditing ? <> <p>hello</p></> :
+                        <>
+                            <li>
+                                <p id={todoItem.id} className={todoItem.completed ? 'completed' : 'not-completed'}>
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => toggleTodoCompleted(todoItem.id)}
+                                    >
+                                    </input>
+                                    {todoItem.todoDescription}
+                                </p>
+                                <button
+                                    onClick={() => updateEditingStatus(todoItem.id)}
+                                >Edit</button>
+                                <button
+                                    onClick={() => deleteTodo(todoItem.id)}
+                                >Delete</button>
+                            </li>
+                        </>
+
+
+                )}
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <label type="text" name="New To Do" for="new_to_do">
+                    <p>New Todo</p>
+                    <input type="text" name="new_to_do" placeholder="To Do" value={input} onChange={(event) => setInput(event.target.value)}></input>
+                </label>
+                <button id="add-new-task" onClick={() => setTodos([...todos, { todoDescription: input, id: nanoid() }])}>add to do</button>
+            </form>
+
+        </>
+    );
 }
 
 export default Todo;
